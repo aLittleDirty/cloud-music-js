95a66046d39976e87134295bbd468c9b37e919f2
peng <1826493691@qq.com>
2019-12-27 15:50:19 +0800

    [feat]修改音乐播放器类的属性

--- src/model/music-player.js
+++ src/model/music-player.js
@@ -1,11 +1,35 @@
 export class MusicPlayer {
-  constructor (playing) {
+  constructor (playing, music, musicList) {
     this.playing = playing
+    this.music = music
+    this.musicList = musicList
   }
   stop () {
     this.playing = false
   }
   play () {
     this.playing = true
+  }
+  next () {
+    let currentIndex = this.musicList.findIndex(this.music)
+    currentIndex++
+    if (currentIndex > this.musicList.length) {
+      currentIndex--
+    }
+    this.music = this.musicList[currentIndex]
+  }
+  prev () {
+    let currentIndex = this.musicList.findIndex(this.music)
+    currentIndex--
+    if (currentIndex < 0) {
+      currentIndex = 0
+    }
+    this.music = this.musicList[currentIndex]
+  }
+  refreshMusicList (newMusicList) {
+    this.musicList = newMusicList
+  }
+  refreshMusic (newMusic) {
+    this.music = newMusic
   }
 }
