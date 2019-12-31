6c51d6e80b7160fea1d3027124e58ae4f6b01619
peng <1826493691@qq.com>
2019-12-31 15:56:27 +0800

    [refactor]音乐播放器组件获取音乐信息

--- src/components/musicPlayer/player.js
+++ src/components/musicPlayer/player.js
@@ -1,5 +1,5 @@
 import { mapGetters } from 'vuex'
-import { getMusicList, getMusicURLs } from '../../api/music-api.js'
+import { getMusicMessage } from '../../api/music-api.js'
 import { Music } from '../../model/music.js'
 import { Singer } from '../../model/singer.js'
 import { formatSeconds } from '../../util/second-format.js'
@@ -28,21 +28,15 @@ export default {
   methods: {
     refreshMusic (newId) {
       // 更新音乐信息
-      let musicMessage = getMusicList(newId.toString())
-      let musicUrl = getMusicURLs(newId.toString())
-      musicMessage.then((result) => {
-        let message = result[0]
+      let musicMessage = getMusicMessage(newId)
+      musicMessage.then((message) => {
         let music = new Music(message)
         let singer = new Singer(message)
         let album = new Album(message)
         this.musicName = music.name
+        this.musicUrl = music.url
         this.singer = singer.name
         this.imageUrl = album.image
-      })
-      musicUrl.then((result) => {
-        // 应该从music实例中获取
-        let musicUrl = result[0].url
-        this.musicUrl = musicUrl
       })
     },
     setCurrentTime () {
