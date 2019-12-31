3a6a87e053920aa24674ce143ccd67488f784959
peng <1826493691@qq.com>
2019-12-31 16:59:03 +0800

    [fixbug]修改存储工具函数中的存储键类型

--- src/util/respository.js
+++ src/util/respository.js
@@ -2,11 +2,13 @@
 let storage = window.localStorage
 
 export function localSet (key, data) {
+  key = key.toString()
   let jsonData = JSON.stringify(data)
   storage.setItem(key, jsonData)
 }
 
 export function localGet (key) {
+  key = key.toString()
   let jsonData = storage.getItem(key)
   return JSON.parse(jsonData)
 }
