6c9a80dbffb84fd922a0aa4abcd18f9ce6a69860
peng <1826493691@qq.com>
2019-12-31 11:06:56 +0800

    [feat]完成音乐播放栏数据渲染

--- src/components/musicPlayer/index.vue
+++ src/components/musicPlayer/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class = "music-player">
-    <router-link to = "/Lyric">{{message}}</router-link>
+    <audio ref="audio" @timeupdate="setCurrentTime" @canplay="setDuration" :src="musicUrl"></audio>
+    <router-link to = "/Lyric">
+      <img :src="imageUrl"/>
+    </router-link>
+    <div class="musicMesssage">
+      <p>{{musicName}} - <span>{{singer}}</span></p>
+      <p>{{currentTime}} / {{duration}}</p>
+    </div>
+    <div class="controlers">
+      <button>上一首</button>
+      <button>暂停 / 播放</button>
+      <button>下一首</button>
+    </div>
   </div>
 </template>
 <script src = "./player.js"></script>
--- src/components/musicPlayer/player.js
+++ src/components/musicPlayer/player.js
@@ -1,10 +1,20 @@
 import { mapGetters } from 'vuex'
 import { getMusicList, getMusicURLs } from '../../api/music-api.js'
+import { Music } from '../../model/music.js'
+import { Singer } from '../../model/singer.js'
+import { formatSeconds } from '../../util/second-format.js'
+import { Album } from '../../model/album.js'
 export default {
   name: 'musicPlayer',
   data () {
     return {
-      message: this.$store.state.musicId
+      musicName: '',
+      singer: '',
+      currentTime: '00 : 00',
+      duration: '',
+      imageUrl: '',
+      playing: false,
+      musicUrl: ''
     }
   },
   computed: {
@@ -20,8 +30,30 @@ export default {
       // 更新音乐信息
       let musicMessage = getMusicList(newId.toString())
       let musicUrl = getMusicURLs(newId.toString())
-      console.log(musicMessage)
-      console.log(musicUrl)
+      musicMessage.then((result) => {
+        let message = result[0]
+        let music = new Music(message)
+        let singer = new Singer(message)
+        let album = new Album(message)
+        this.musicName = music.name
+        this.singer = singer.name
+        this.imageUrl = album.image
+      })
+      musicUrl.then((result) => {
+        // 应该从music实例中获取
+        let musicUrl = result[0].url
+        this.musicUrl = musicUrl
+      })
+    },
+    setCurrentTime () {
+      this.currentTime = formatSeconds(this.$refs.audio.currentTime)
+    },
+    setDuration () {
+      this.duration = formatSeconds(this.$refs.audio.duration)
     }
+  },
+  created () {
+    let id = this.$store.state.musicId
+    this.refreshMusic(id)
   }
 }
