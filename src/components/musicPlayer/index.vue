39514f836c16402142e9aa7afa4e86594f5e682c
peng <1826493691@qq.com>
2020-01-04 01:02:33 +0800

    [style]音乐播放栏样式设置

--- src/components/musicPlayer/index.vue
+++ src/components/musicPlayer/index.vue
@@ -1,22 +1,24 @@
 <template>
   <div class = "music-player">
     <audio ref="audio"
-    @timeupdate="setCurrentTime"
-    @canplay="setDuration"
-    :src="musicUrl"></audio>
-    <router-link to = "/Lyric">
-      <img :src="imageUrl"/>
-    </router-link>
-    <div class="musicMesssage">
+      @timeupdate="setCurrentTime"
+      @canplay="setDuration"
+      :src="musicUrl">
+    </audio>
+    <div class="musicMessage">
+      <router-link to="/Lyric">
+        <img class="albumImage" :src="imageUrl"/>
+      </router-link>
       <p>{{musicName}} - <span>{{singer}}</span></p>
       <p>{{currentTime}} / {{duration}}</p>
     </div>
-    <div class="controlers">
+    <div class="controllers">
       <button @click="prev">上一首</button>
       <button @click="swap">暂停 / 播放</button>
       <button @click="next">下一首</button>
     </div>
-    <input type="range" value="5" max="10" min="0" @input="changeVolume"/>
+    <input class="volume" type="range" value="5" max="10" min="0" @input="changeVolume"/>
   </div>
 </template>
 <script src = "./player.js"></script>
+<style src="./music-player.css" scoped></style>
