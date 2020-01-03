c2e89f82f0e594222949c0a1092766edf79ad88c
peng <1826493691@qq.com>
2020-01-03 16:16:18 +0800

    [style]完成排行榜组件的样式设置

--- src/components/billboard/index.vue
+++ src/components/billboard/index.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class = "billboard">
-    <div key="loading" v-if="loading">
+  <div class="billboard">
+    <div v-if="loading" class="loading" key="loading">
       <img src="../../assets/loading.png"/>
     </div>
-    <div key="loaded" v-else>
-      <router-link v-for="(item, index) in lists" :key="index"
-      :to="{ path: '/musicList', query: {id:item.id}}">
-        <img :src="item.coverImg">
-        <p>{{item.name}}</p>
-      </router-link>
-    </div>
+    <ul v-else class="loaded" key="loaded">
+      <li v-for="(item, index) in lists" :key="index">
+        <router-link :to="{ path: '/musicList', query: {id:item.id}}">
+          <img class="billboardImg" :src="item.coverImg">
+          <p class="billboardName">{{item.name}}</p>
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
 <script src = "./billboard.js">
