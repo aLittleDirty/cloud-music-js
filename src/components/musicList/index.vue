7511682d9021f0af20ebee61019277f2f3076f69
peng <1826493691@qq.com>
2020-01-03 19:48:26 +0800

    [style]音乐列表样式设置

--- src/components/musicList/index.vue
+++ src/components/musicList/index.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="music-list">
-    <div class = "billboard">
-    <div key="loading" v-if="loading">
+    <div v-if="loading" class="loading" key="loading">
       <img src="../../assets/loading.png"/>
     </div>
-      {{message}}
-      <table>
+    <table v-else class="loaded, table">
+      <thead class="thead">
         <tr>
           <th>音乐标题</th>
           <th>歌手</th>
           <th>专辑</th>
           <th>时长</th>
         </tr>
+      </thead>
+      <tbody class="table-item" >
         <tr v-for="(item, index) in musicList" :key="index" @click="setMusicId(item.id)">
           <td>{{item.name}}</td>
           <td>{{item.singer}}</td>
           <td>{{item.albumName}}</td>
-          <td>{{item.duration}}</td>
+          <td class="time">{{item.duration}}</td>
         </tr>
-      </table>
-    </div>
+      </tbody>
+    </table>
   </div>
 </template>
 <script src = "./music-list.js"></script>
+<style src="./music-list.css" scoped></style>
