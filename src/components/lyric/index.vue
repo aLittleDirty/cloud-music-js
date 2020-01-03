31a7805757115b1ac4cf474910aa775746c63aba
peng <1826493691@qq.com>
2020-01-03 18:20:13 +0800

    [style]歌词页组件样式设置

--- src/components/lyric/index.vue
+++ src/components/lyric/index.vue
@@ -1,16 +1,18 @@
 <template>
   <div class = "lyric">
-    <div v-if="loading" key="loading">
+    <div v-if="loading" class="loading" key="loading">
       <img src="../../assets/loading.png"/>
     </div>
     <section v-else class="loaded" key="loaded">
-      <img :src="albumImg"/>
-      <h1>{{musicName}}</h1>
-      <p>专辑：{{albumName}} 歌手： {{singer}}</p>
-      <div class="wrapper" ref="wrapper">
-        <ul class="content" ref="content">
-          <li v-for="(item, index) in lyric.lines" :key="index">{{item.txt}}</li>
-        </ul>
+      <img class="albumImage" :src="albumImg"/>
+      <div class="lyric-content">
+        <h1>{{musicName}}</h1>
+        <p>专辑：{{albumName}} 歌手： {{singer}}</p>
+        <div class="wrapper" ref="wrapper">
+          <ul class="content" ref="content">
+            <li v-for="(item, index) in lyric.lines" :key="index">{{item.txt}}</li>
+          </ul>
+        </div>
       </div>
     </section>
   </div>
