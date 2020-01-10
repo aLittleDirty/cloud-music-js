8aeae67fc04e68919b26f088bc3e487152b41f11
peng <1826493691@qq.com>
2020-01-10 20:07:00 +0800

    [feat]榜单页信息设置本地存储，并在首次获取榜单信息时创建榜单数据库

--- src/components/billboard/billboard.js
+++ src/components/billboard/billboard.js
@@ -1,5 +1,6 @@
 import { Billboard } from '../../model/billboard.js'
 import { getBillboard } from '../../api/music-api.js'
+import { localGet, localSet, initDataBase } from '../../util/repository.js'
 export default {
   name: 'billboard',
   data () {
@@ -9,12 +10,24 @@ export default {
     }
   },
   created () {
-    getBillboard().then((billboardList) => {
-      for (let i = 0; i < billboardList.length; i++) {
-        let billboard = new Billboard(billboardList[i])
-        this.lists.push(billboard)
-      }
-      this.loading = false
-    })
+    let billboardLists = localGet('billboardLists')
+    if (billboardLists) {
+      this.lists = billboardLists
+    } else {
+      getBillboard().then((billboardList) => {
+        let lists = []
+        let storeNameList = []
+        for (let i = 0; i < billboardList.length; i++) {
+          let billboard = new Billboard(billboardList[i])
+          lists.push(billboard)
+          storeNameList.push(billboard.name)
+        }
+        localSet('billboardLists', lists)
+        this.lists = lists
+        this.loading = false
+        // 创建数据库
+        initDataBase('cloud-music', storeNameList, 'id', 'name')
+      })
+    }
   }
 }
