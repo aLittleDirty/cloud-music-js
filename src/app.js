0ec8af3ceb471b6b0cd4259ace309d1d8e52fb06
peng <1826493691@qq.com>
2020-02-07 23:11:58 +0800

    [feat]添加登录组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Header from './components/header/index.vue'
 import MusicPlayer from './components/musicPlayer/index.vue'
+import Login from './components/login/index.vue'
 export default {
   name: 'app',
   components: {
     Header,
-    MusicPlayer
+    MusicPlayer,
+    Login
   }
 }
