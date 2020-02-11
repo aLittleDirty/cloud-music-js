0847732bd410409f1acd00281b761af5d4347c3c
peng <1826493691@qq.com>
2020-02-12 00:58:10 +0800

    [feat]设置header组件样式

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="header">
-    <router-link to = "/">主页</router-link>
-    {{currentPage}}
+    <nav>
+      <li class="logo">
+        <img src="../../assets/网易云音乐logo.jpeg"/>
+        <h1>网易云音乐</h1>
+      </li>
+      <li :class="[{'focus': focusOption === 1}, 'billboard']" @click="focusOption = 1">
+        <router-link to = "/">{{currentPage}}</router-link>
+      </li>
+      <li :class="[{'focus': focusOption === 2}, 'my-music']" @click="focusOption = 2">我的音乐</li>
+      <li class="login">
+        <Login/>
+      </li>
+    </nav>
   </div>
 </template>
 <script src = "./header.js"></script>
