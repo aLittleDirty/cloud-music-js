0cd80215f8dcc4a53a1baf0dfa3344f7ced9136f
peng <1826493691@qq.com>
2020-02-13 11:40:45 +0800

    [feat]添加下拉选项栏

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="login">
     <button v-if="!isLogin" id="unlog" class="unlogIcon" @click="showLoginBox = true">登录</button>
-    <div v-else id="userMessage" class="userMessage">
-      <img :src="userIcon"/>
-      <span>{{userName}}</span>
+    <div v-else class="userMessage">
+      <img class="userIcon" :src="userIcon"/>
+      <ul class="userMenu">
+        <li class="userDetail">
+          <img src="../../assets/用户.png"/>
+          <span>{{userName}}</span>
+        </li>
+        <li class="logout">
+          <img src="../../assets/退出登录.png"/>
+          <button @click="logout">退出</button>
+        </li>
+      </ul>
     </div>
-    <button class="logout" @click="logout">退出</button>
-    <div class="loginBox" v-show="showLoginBox">
-      <span>手机号登录</span>
-      <button @click="showLoginBox = false">X</button>
-      <input v-model="userPhone" placeholder="请输入你的手机号"/>
-      <input v-model="passWord" placeholder="请输入密码"/>
+    <div class="dialog" v-show="showLoginBox">
+      <div class="dialogHandle">
+        <span>手机号登录</span>
+        <button @click="showLoginBox = false">X</button>
+      </div>
+      <input class="phone" type="number" v-model="userPhone" placeholder="请输入你的手机号"/>
+      <input class="password" type="password" v-model="passWord" placeholder="请输入密码"/>
       <button class="loginBtn" @click="login(userPhone, passWord)">登录</button>
     </div>
   </div>
