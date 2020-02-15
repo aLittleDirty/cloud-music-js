9bc40d2bf17e1dcc80b39306454afac25549dd89
peng <1826493691@qq.com>
2020-02-15 23:19:49 +0800

    [rename]修改css样式命名

--- src/components/paging/index.vue
+++ src/components/paging/index.vue
@@ -5,7 +5,7 @@
     <button
     v-for="(item, index) in pages"
     :key="index"
-    :class="[{ highLight: currentPage === index },'page-number']"
+    :class="[{ highlight: currentPage === index },'page-number']"
     @click="gotoPage(index)">{{index + 1}}</button>
     <button class="next" @click="(currentPage !== pages - 1) && gotoPage(currentPage + 1)">下一页</button>
     <button class="last" @click="gotoPage(pages - 1)">尾页</button>
