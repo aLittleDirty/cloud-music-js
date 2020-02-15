a582aa4bd7bb6de23ca98f55a3475c0762354445
peng <1826493691@qq.com>
2020-02-16 00:03:21 +0800

    [rename]修改css样式命名

--- src/components/banner/index.vue
+++ src/components/banner/index.vue
@@ -3,7 +3,7 @@
     <transition-group name="fade-img" mode="out-in">
       <img class="fade-img" v-for="(page, index) in pages" :key="index" :src="page" v-show="currentPage === index" />
     </transition-group>
-    <ul class="spotList">
+    <ul class="spot-list">
       <li :class="[{'currentSpot': index === currentPage},'spot']" v-for="(value, index) in pages.length" :key="pages.length[index]" @click="toPage(index)">{{value}}</li>
     </ul>
   </div>
