7a014ba8e5abed15e7dd4c06bc04ca4926fd2377
peng <1826493691@qq.com>
2020-02-16 00:04:17 +0800

    [rename]修改样式命名

--- src/components/banner/index.vue
+++ src/components/banner/index.vue
@@ -4,7 +4,7 @@
       <img class="fade-img" v-for="(page, index) in pages" :key="index" :src="page" v-show="currentPage === index" />
     </transition-group>
     <ul class="spot-list">
-      <li :class="[{'currentSpot': index === currentPage},'spot']" v-for="(value, index) in pages.length" :key="pages.length[index]" @click="toPage(index)">{{value}}</li>
+      <li :class="[{'current-spot': index === currentPage},'spot']" v-for="(value, index) in pages.length" :key="pages.length[index]" @click="toPage(index)">{{value}}</li>
     </ul>
   </div>
 </template>
