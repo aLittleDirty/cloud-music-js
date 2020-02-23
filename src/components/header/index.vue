9a0aa3a3b68d4f5768828f42060c9c3c5a413368
peng <1826493691@qq.com>
2020-02-23 20:34:53 +0800

    [feat]添加路由

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -6,9 +6,11 @@
         <h1>网易云音乐</h1>
       </li>
       <li :class="[{'focus': focusOption === 1}, 'billboard']" @click="focusOption = 1">
-        <router-link to = "/">{{currentPage}}</router-link>
+        <router-link to = "/billboard">{{currentPage}}</router-link>
       </li>
-      <li :class="[{'focus': focusOption === 2}, 'private-music']" @click="focusOption = 2">我的音乐</li>
+      <li :class="[{'focus': focusOption === 2}, 'private-music']" @click="focusOption = 2">
+        <router-link to="/personal">我的音乐</router-link>
+      </li>
       <li class="login">
         <Login/>
       </li>
