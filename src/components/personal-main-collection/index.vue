7bc30d409185260a1cf85627be7ac0ce9b4e62e8
peng <1826493691@qq.com>
2020-02-23 20:38:33 +0800

    [feat]添加我的音乐--收藏列表组件

---
+++ src/components/personal-main-collection/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="personal-main-collection"></div>
+</template>
+<script src="./personal-main-collection.js"></script>
+<style scoped src="./personal-main-collection.css"></style>
