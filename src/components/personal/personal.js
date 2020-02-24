494e048fbf3eca5fd570b986216a565db176f844
peng <1826493691@qq.com>
2020-02-24 20:05:45 +0800

    [fix]将用户id添加到组件的响应式数据中

--- src/components/personal/personal.js
+++ src/components/personal/personal.js
@@ -7,15 +7,16 @@ export default {
     return {
       selectionsList: [],
       signersCount: 0,
-      radiosCount: 0
+      radiosCount: 0,
+      userId: ''
     }
   },
   components: {
     PersonalSidebarSelections
   },
   async mounted () {
-    let userId = localGet('userId')
-    let [ [ createdPlaylist, collectedPlaylist ], { signersCount, radiosCount, createdPlaylistCount, collectedPlaylistCount } ] = await Promise.all([getPersonalPlaylists(userId), getPersonalOptionsCount()])
+    this.userId = localGet('userId')
+    let [ [ createdPlaylist, collectedPlaylist ], { signersCount, radiosCount, createdPlaylistCount, collectedPlaylistCount } ] = await Promise.all([getPersonalPlaylists(this.userId), getPersonalOptionsCount()])
     this.signersCount = signersCount
     this.radiosCount = radiosCount
     this.selectionsList = [ { title: `创建的歌单(${createdPlaylistCount})`, playlist: createdPlaylist }, { title: `收藏的歌单(${collectedPlaylistCount})`, playlist: collectedPlaylist } ]
