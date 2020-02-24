20e177e23a2cb9ccef2c96192faf729ea7d9568d
peng <1826493691@qq.com>
2020-02-24 20:06:46 +0800

    [delete]删除错误命名的文件

--- src/components/personal-main-collection/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="personal-main-collection"></div>
-</template>
-<script src="./personal-main-collection.js"></script>
-<style scoped src="./personal-main-collection.css"></style>
--- src/components/personal-main-collection/personal-main-collection.js
+++
@@ -1,7 +0,0 @@
-export default {
-  name: 'personal-main-collection',
-  data () {
-    return {
-    }
-  }
-}
